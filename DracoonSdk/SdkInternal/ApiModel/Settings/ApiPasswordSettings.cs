5381b5e5c04506bb670eccc112434d478684a1dc
Andreas Zirngibl <a.zirngibl@dracoon.com>
2019-09-27 09:44:16 +0200

    Add api model for password policies
    See: #SDKCSHARP-89

---
+++ DracoonSdk/SdkInternal/ApiModel/Settings/ApiPasswordSettings.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Dracoon.Sdk.SdkInternal.ApiModel {
+    internal class ApiPasswordSettings {
+        [JsonProperty("loginPasswordPolicies", NullValueHandling = NullValueHandling.Ignore)]
+        public ApiLoginPasswordSettings LoginPasswordSettings { get; set; }
+        [JsonProperty("sharesPasswordPolicies", NullValueHandling = NullValueHandling.Ignore)]
+        public ApiSharePasswordSettings SharePasswordSettings { get; set; }
+        [JsonProperty("encryptionPasswordPolicies", NullValueHandling = NullValueHandling.Ignore)]
+        public ApiEncryptionPasswordSettings EncryptionPasswordSettings { get; set; }
+    }
+}
